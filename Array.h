7881626ba96464ab2f90cad4c98b81a12495d5a5
ZhouDaxia929 <zhoudongxu929@163.com>
2018-06-28 23:14:50 +0800

    first

--- Array.h
+++ Array.h
@@ -12,7 +12,21 @@ protected:
     unsigned int length;
 public:
     Array();
+    Array(unsigned int, unsigned int = 0);
+    ~Array();
 
+    Array(Array const&);
+    Array& operator = (Array const&);
+
+    T const& operator [] (unsigned int) const;
+    T& operator [] (unsigned int);
+
+    T const* Data() const;
+    unsigned int Base() const;
+    unsigned int Length() const;
+
+    void SetBase(unsigned int);
+    void SetLength(unsigned int);
 };
 
 #endif //DS_ARRAY_H
