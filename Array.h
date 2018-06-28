17070d024da1397b4ad0ada034654781965f8a77
ZhouDaxia929 <zhoudongxu929@163.com>
2018-06-28 21:41:51 +0800

    Initial commit

---
+++ Array.h
@@ -0,0 +1,18 @@
+//
+// Created by ZhouDaxia on 2018/6/28.
+//
+
+#ifndef DS_ARRAY_H
+#define DS_ARRAY_H
+template <class T>
+class Array{
+protected:
+    T* data;
+    unsigned int base;
+    unsigned int length;
+public:
+    Array();
+
+};
+
+#endif //DS_ARRAY_H
